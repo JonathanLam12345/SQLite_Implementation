e62b281c52149e5e91c4418461a79465ed353174
socero <general@socero.com>
2017-05-10 16:34:59 -0400

    hi

---
+++ app/src/main/java/com/example/jonathanlam/sqlite_slidenerd/JonathanHelper.java
@@ -0,0 +1,12 @@
+package com.example.jonathanlam.sqlite_slidenerd;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+/**
+ * Created by Jonathan Lam on 2017-05-10.
+ */
+
+public class JonathanHelper
+{
+}
