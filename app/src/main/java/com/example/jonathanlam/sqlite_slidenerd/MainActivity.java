d8bab7990f60c854729ad3882a773b4324f9d84f
socero <general@socero.com>
2017-05-11 14:14:39 -0400

    :)

--- app/src/main/java/com/example/jonathanlam/sqlite_slidenerd/MainActivity.java
+++ app/src/main/java/com/example/jonathanlam/sqlite_slidenerd/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.jonathanlam.sqlite_slidenerd;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,5 +15,17 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         jonathanHelper = new JonathanHelper(this);
+
+
+
+        /*
+        SQLite is efficient because it avoid the database creation overhead by
+        creating the database only when someone tries accessing it for the first time.
+        By initializing JonathanHelper, we have not yet accessed the database,
+        to access the database object that represents the physical database file stored
+        on our device, called the getWriteDatabase() method and that ll trigger the
+        other lifecycle methods of SQLiteOpenHelper.
+        */
+        SQLiteDatabase sqLiteDatabase = jonathanHelper.getWritableDatabase();
     }
 }
